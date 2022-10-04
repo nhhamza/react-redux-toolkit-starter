fa695ab1f29b33ab8e62fc953ab6f0f5706a94b0
Hamza Lhimer <hamza.lhimer@cssregtech.com>
2022-10-04 17:51:19 +0200

    Initial Commit

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { API_URI } from "../../../envs/envUtil";
 
 function App() {
-  console.log("API_URI", API_URI);
+  console.log("API_URI2", API_URI);
 
   return <h1 role="h1">Hello world React!</h1>;
 }
