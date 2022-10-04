070c929d2fe7653272b8c3d8324f04eb67e8b1fc
Hamza Lhimer <hamza.lhimer@cssregtech.com>
2022-10-04 17:57:02 +0200

    test

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { API_URI } from "../../../envs/envUtil";
 
 function App() {
-  console.log("API_URI", API_URI);
+  console.log("API_URI2", API_URI);
 
   return <h1 role="h1">Hello world React!</h1>;
 }
